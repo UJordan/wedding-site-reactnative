1f0bb2bd1104eac49b7d1a045ac22d002fa73c6d
Christa <jaysplaypen@gmail.com>
2023-03-17 10:06:20 -0700

    stack and reservation

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -1,103 +1,235 @@
-import { StyleSheet, View, Text, Button } from 'react-native';
-// import { createStackNavigator } from '@react-navigation/stack';
-import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList, DrawerItem } from '@react-navigation/drawer';
-import { NavigationContainer, useNavigation } from '@react-navigation/native';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
-import 'react-native-gesture-handler';
-import HomeScreen from './HomeScreen';
-import ContactScreen from './ContactScreen';
-import InfoScreen from './InfoScreen';
-import ReservationScreen from './ReservationScreen';
-import Gallery from './Gallery.js';
-import CustomDrawer from '../components/CustomDrawer';
+import { StyleSheet, View, Platform, Text, Button } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { Icon } from "react-native-elements";
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
+import Constants from "expo-constants";
+import "react-native-gesture-handler";
+import HomeScreen from "./HomeScreen";
+import ContactScreen from "./ContactScreen";
+import InfoScreen from "./InfoScreen";
+import ReservationScreen from "./ReservationScreen";
+import Gallery from "./Gallery.js";
+import CustomDrawer from "../components/CustomDrawer";
 
 const Drawer = createDrawerNavigator();
 
+const screenOptions = {
+  headerTintColor: "#fff",
+  headerStyle: { backgroundColor: "#b8c5d1" },
+  headerShown: true,
+};
+
+const HomeNavigator = () => {
+  const Stack = createStackNavigator();
+  return (
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={({ navigation }) => ({
+          title: "Home",
+          headerLeft: () => (
+            <Icon
+              name="home"
+              type="font-awesome"
+              iconStyle={styles.stackIcon}
+              onPress={() => navigation.toggleDrawer()}
+            />
+          ),
+        })}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const InfoNavigator = () => {
+  const Stack = createStackNavigator();
+  return (
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen
+        name="Info"
+        component={InfoScreen}
+        options={({ navigation }) => ({
+          headerLeft: () => (
+            <Icon
+              name="info-circle"
+              type="font-awesome"
+              iconStyle={styles.stackIcon}
+              onPress={() => navigation.toggleDrawer()}
+            />
+          ),
+        })}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const ReservationNavigator = () => {
+  const Stack = createStackNavigator();
+  return (
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen
+        name="Reservation"
+        component={ReservationScreen}
+        options={({ navigation }) => ({
+          title: "Reservation",
+          headerLeft: () => (
+            <Icon
+              name="calendar-check-o"
+              type="font-awesome"
+              iconStyle={styles.stackIcon}
+              onPress={() => navigation.toggleDrawer()}
+            />
+          ),
+        })}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const GalleryNavigator = () => {
+  const Stack = createStackNavigator();
+  return (
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen
+        name="Favorites"
+        component={Gallery}
+        options={({ navigation }) => ({
+          title: "Gallery",
+          headerLeft: () => (
+            <Icon
+              name="camera"
+              type="font-awesome"
+              iconStyle={styles.stackIcon}
+              onPress={() => navigation.toggleDrawer()}
+            />
+          ),
+        })}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const ContactNavigator = () => {
+  const Stack = createStackNavigator();
+  return (
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen
+        name="Contact"
+        component={ContactScreen}
+        options={({ navigation }) => ({
+          title: "Contact Us",
+          headerLeft: () => (
+            <Icon
+              name="address-card"
+              type="font-awesome"
+              iconStyle={styles.stackIcon}
+              onPress={() => navigation.toggleDrawer()}
+            />
+          ),
+        })}
+      />
+    </Stack.Navigator>
+  );
+};
+
 const Main = () => {
-    return (
-        <Drawer.Navigator 
-            drawerContent={props => <CustomDrawer {...props} />} 
-            initialRouteName='Home' 
-            screenOptions={{
-                headerShown: true, 
-                // drawerActiveBackgroundColor: '#b8c5d1',
-                // drawerActiveTintColor: '#fff',
-                // drawerInactiveTintColor: '#3333',
-                // drawerLabelStyle: {
-                //     marginLeft: -50,
-                //     fontSize: 20
-                // }
-            }}
-        >
-            <Drawer.Screen name="Home" component={HomeScreen} options={{
-                drawerIcon: ({color}) => (
-                    <Ionicons name='home' size={22} color={color} />
-                )
-            }} />
-            <Drawer.Screen name="Information" component={InfoScreen} options={{
-                drawerIcon: ({color}) => (
-                    <FontAwesome5 name='info-circle' size={22} color={color} />
-                )
-            }}/>
-            <Drawer.Screen name="Reservation" component={ReservationScreen} options={{
-                drawerIcon: ({color}) => (
-                    <FontAwesome5 name='calendar-check' size={22} color={color} />
-                )
-            }}/>
-            <Drawer.Screen name="Gallery" component={Gallery} options={{
-                drawerIcon: ({color}) => (
-                    <FontAwesome5 name='images' size={22} color={color} />
-                )
-            }}/>
-            <Drawer.Screen name="Contact" component={ContactScreen} options={{
-                drawerIcon: ({color}) => (
-                    <FontAwesome5 name='envelope-open-text' size={22} color={color} />
-                )
-            }} />
-        </Drawer.Navigator>
-    )
-}
+  return (
+    <View
+      style={{
+        flex: 1,
+        paddingTop: Platform.OS === "ios" ? 0 : Constants.statusBarHeight,
+      }}>
+      <Drawer.Navigator
+        drawerContent={(props) => <CustomDrawer {...props} />}
+        initialRouteName="Home"
+        drawerStyle={{ backgroundColor: "#b8c5d1" }}>
+        <Drawer.Screen
+          name="Home"
+          component={HomeNavigator}
+          options={{
+            drawerIcon: ({ color }) => (
+              <Ionicons
+                name="home"
+                size={22}
+                iconStyle={{ width: 22 }}
+                color={color}
+              />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="Information"
+          component={InfoNavigator}
+          options={{
+            drawerIcon: ({ color }) => (
+              <FontAwesome5
+                name="info-circle"
+                size={22}
+                iconStyle={{ width: 22 }}
+                color={color}
+              />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="Reservation"
+          component={ReservationNavigator}
+          options={{
+            drawerIcon: ({ color }) => (
+              <FontAwesome5
+                name="calendar-check"
+                size={22}
+                color={color}
+              />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="Gallery"
+          component={GalleryNavigator}
+          options={{
+            drawerIcon: ({ color }) => (
+              <FontAwesome5
+                name="images"
+                size={22}
+                color={color}
+              />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="Contact"
+          component={ContactNavigator}
+          options={{
+            drawerIcon: ({ color }) => (
+              <FontAwesome5
+                name="envelope-open-text"
+                size={22}
+                color={color}
+              />
+            ),
+          }}
+        />
+      </Drawer.Navigator>
+    </View>
+  );
+};
 
-// const screenOptions = {
-//     headerTintColor: '#fff',
-//     headerStyle: { backgroundColor: '#5637DD'}
-// }
-
-// const HomeNavigator = () => {
-//     const Stack = createStackNavigator();
-
-//     return (
-//         <Stack.Navigator screenOptions={screenOptions}>
-//             <Stack.Screen 
-//                 name='Home'
-//                 component={HomeScreen}
-//                 options={{ title: 'Home'}}
-//             />
-//         </Stack.Navigator>
-//     )
-// }
-
-// const InfoNavigator = () => {
-//     const Stack = createStackNavigator();
-//     return (
-//         <Stack.Navigator screenOptions={screenOptions}>
-//             <Stack.Screen 
-//                 name='Info'
-//                 component={InfoScreen}
-//                 options={{ title: 'Info'}}
-//             />
-//         </Stack.Navigator>
-//     )
-// }
-
-// const styles = StyleSheet.create({
-//     container: {
-//       flex: 1,
-//       backgroundColor: '#fff',
-//       alignItems: 'center',
-//       justifyContent: 'center',
-//     },
-//   });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  stackIcon: {
+    marginLeft: 10,
+    color: "#fff",
+    fontSize: 22,
+  },
+});
 
 export default Main;
